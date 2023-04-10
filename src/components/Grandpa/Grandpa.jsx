7fc3f4eda61b15a68e82b5d16117a6ac6b006dfe
belaeat <omyhossain@gmail.com>
2023-04-11 04:04:41 +0600

    learned context api

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css'
 
+// context name
+export const RingContext = createContext('gold');
+
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
     const ring = 'diamond';
+
+    const [money, setMoney] = useState(0);
 
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
+            <p>Has Money: {money}</p>
+
+            {/* 2. use of context provider */}
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='golden Ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
 
 export default Grandpa;
+
+
+/**
+ * HOW TO USE CONTEXT API
+ * 
+ * 1. Create a context and export it
+ * 2. create a provider and pass a value
+ * 3. use useContext to receive a value
+ * 
+*/
--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ring}) => {
+
+    // use useContext to receive a value
+    const angti = useContext(RingContext);
+    
     return (
         <div>
             <h2>Special</h2>
-            <p><small>Ring: {ring}</small></p>
+            <p><small>Ring: {angti}</small></p>
         </div>
     );
 };
